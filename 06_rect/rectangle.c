deea42fb07bb2afe4fe4fe8eb1c32a50c94dcf17
Coursera Learner <nobody@nowhere.nul>
2018-07-26 17:31:03 +0000

    AA

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,15 +16,59 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+struct rect {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+ typedef struct rect rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if(r.width < 0) {
+   r.x = r.x + r.width;
+   r.width=0-r.width;}
+    
+      
+  if (r.height < 0){
+   
+    r.y=r.y+r.height;
+    r.height=0-r.height;
+  }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
-  return r1;
+  rectangle ans;
+ 
+  r1=canonicalize(r1);
+  r2=canonicalize(r2);  
+  ans.x=max(r1.x,r2.x);
+   ans.y=max(r1.y,r2.y);
+  int tem;
+  int temy;
+  tem= min(r1.x,r2.x);
+  temy=min(r1.y,r2.y);
+  if (((tem == r1.x)&&(r2.x > r1.x + r1.width))||((temy==r1.y)&&(r2.y > r1.y + r1.height))){
+      ans.width=0;
+      ans.height=0;
+      return ans;}
+  if(((tem == r2.x)&&(r1.x > r2.x + r2.width))||((temy==r2.y)&&(r1.y > r2.y + r2.height))){
+      ans.width=0;
+      ans.height=0;
+      return ans;}
+  
+ 
+
+  int minheight=min(r1.y+r1.height,r2.y+r2.height);
+  int minwidth=min(r1.x+r1.width,r2.x+r2.width);
+  ans.height=minheight - ans.y;
+  if(minheight <0) ans.height=minheight + ans.y;
+  
+  ans.width=minwidth-ans.x;
+  if (minwidth < 0 ) ans.width=minwidth+ans.x;
+  return ans;
 }
 
 //You should not need to modify any code below this line
