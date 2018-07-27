e5f3af1de2ea48b90fdf5cf0685dc16896f3089a
Coursera Learner <nobody@nowhere.nul>
2018-07-27 14:17:32 +0000

    asasa

---
+++ 07_retirement/retirement.c
@@ -0,0 +1,45 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct{
+  int months;
+  double contribution;
+  double Rate_of_return;
+}retire_info;
+
+double cal(double acc , retire_info st ){
+  return acc*st.Rate_of_return + st.contribution + acc;
+}
+void retirement (int startAge ,double initial , retire_info working,retire_info retired){
+  double temp;
+  temp= initial;
+  for (int i=0;i<working.months ; i++,startAge++){
+    printf("Age %3d month %2d you have $%.2lf\n",startAge/12 , startAge%12, temp);
+    temp=cal(temp,working);
+  }
+  for (int i=0;i<retired.months;i++,startAge++){
+    printf("Age %3d month %2d you have $%.2lf\n",startAge/12 , startAge%12, temp);
+    temp=cal(temp,retired);
+  }  
+  //calculate retirement account balabce while working
+  //print the formula
+  //similar task to retirement
+  //print out the same information
+}
+
+
+int main(void){
+  retire_info working;
+  working.months=489;
+  working.Rate_of_return=.045/12;
+  working.contribution=1000;
+  retire_info retired;
+  retired.months=384;
+  retired.Rate_of_return = .01/12;
+  retired.contribution=-4000;
+  int startAge=327;
+  double  initial =21345;
+  retirement(startAge,initial,working,retired);
+  return 0;
+}
