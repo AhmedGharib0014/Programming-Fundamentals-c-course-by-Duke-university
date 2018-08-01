138978240c405f5230d30c1abf791e495a7d32c0
Coursera Learner <nobody@nowhere.nul>
2018-08-01 14:51:58 +0000

    aa

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -17,7 +17,7 @@ const char * ranking_to_string(hand_ranking_t r) {
   case  FOUR_OF_A_KIND: return"FOUR_OF_A_KIND";
   case FULL_HOUSE: return"FULL_HOUSE";
   case  FLUSH: return"FLUSH";
-  case  STRAIGHT: return" STRAIGHT";
+  case  STRAIGHT: return"STRAIGHT";
   case  THREE_OF_A_KIND: return"THREE_OF_A_KIND";
   case  TWO_PAIR: return"TWO_PAIR";
   case  PAIR: return"PAIR";
