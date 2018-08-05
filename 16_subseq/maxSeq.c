dbcc54fe6d58736c0c43914783d0d9bf80e1cbbd
Coursera Learner <nobody@nowhere.nul>
2018-08-05 11:52:03 +0000

    zsd

---
+++ 16_subseq/maxSeq.c
@@ -0,0 +1,16 @@
+
+size_t maxSeq(int * array, size_t n){
+  size_t  lenth= 1;
+  size_t maxlen=0;
+  if (n == 0 ) return 0;
+  
+  for(size_t i=1 ; i<n ;i++ ){
+    if (array[i] > array[i-1]){lenth += 1;}
+    else {
+      if (lenth > maxlen) maxlen = lenth;
+      lenth=1;
+	  } 
+  }
+  if (lenth > maxlen) maxlen = lenth;
+  return maxlen; 
+}
