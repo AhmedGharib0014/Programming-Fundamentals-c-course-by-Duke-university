8668254f0b89a32722cd16fe9663096b76d23f3c
Learn 2 Program Grader <invalid@nowhere.com>
2018-08-05 14:20:49 +0000

    Released assignment

---
+++ 19_bits_arr/numToBits.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int getNthBit(uint32_t number, int bit) {
+  if (bit <0 || bit >= 32) {
+    printf("Bit %d is invalid\n", bit);
+    exit (EXIT_FAILURE);
+  }
+  return (number & (1<<bit)) != 0;
+}
+
+void numToBits(uint32_t * nums, int nNums, int * bits, int nBits) {
+
+}
+
+void doTest(uint32_t * nums, int n) {
+  int bits[n *32];
+  numToBits(nums, n, bits, n*32);
+  for (int i =0; i < n; i++) {
+    printf(" %9d (%8X) => ", nums[i], nums[i]);
+    for (int j = 0; j < 32; j++) {
+      printf("%d", bits[i*32 + j]);
+    }
+    printf("\n");
+  }
+}
+
+int main(void) {
+  uint32_t array1[] = { 1, 2, 3, 4, 5, 15, 109};
+  uint32_t array2[] = { 123456789, 987654321 };
+  int bits[7*32-1];
+  doTest (array1, 7);
+  doTest (array2, 2);
+  numToBits(array1,7, bits , 7*32-1);
+  return EXIT_SUCCESS;
+}
