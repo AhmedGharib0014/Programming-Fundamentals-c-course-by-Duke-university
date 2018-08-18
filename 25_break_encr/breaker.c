f691a347363572504652a494d940c29fd5f7b6d4
Coursera Learner <nobody@nowhere.nul>
2018-08-18 09:59:39 +0000

    asas

---
+++ 25_break_encr/breaker.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int findkey(FILE *f){  
+  int arr[26]={0};
+  int c;
+  
+  while((c=fgetc(f)) != EOF){
+     if (isalpha(c)) {
+       c=tolower(c);
+       c -= 'a';
+       arr[c] ++;
+     }
+  }
+    int max=0;
+     for (int i=0 ;i<26; i++){
+       if(arr[i] > max) max=i;
+     }
+     int k = max -('e' - 'a');
+     return k ;
+   }
+  
+
+  
+
+  
+int main(int argc,char** argv){
+     if (argc != 2 ){
+       fprintf(stderr,"not enough arguments\n");
+       return EXIT_FAILURE;
+     }
+    
+    FILE * f= fopen(argv[1] ,"r");
+
+    if (f == NULL) {
+       perror("Could not open file");
+       return EXIT_FAILURE;      
+    }
+    
+    int key= findkey(f);
+    printf("%d\n",key);
+    
+    if (fclose(f) != 0) {
+       perror("Failed to close the input file!");
+       return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+    
+ }
+
