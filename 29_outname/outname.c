1f1e6ffe057c0948b27833986cbf57ba9df8cc97
Coursera Learner <nobody@nowhere.nul>
2018-08-24 15:31:39 +0000

    asas

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -4,5 +4,10 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
+  char * outname = malloc((strlen(inputName)+4)*sizeof(*outname));
+  strcpy(outname , inputName);
+  outname= realloc(outname ,(strlen(inputName)+10)*sizeof(*outname));
+  strcat(outname,".counts");
+  return outname;
   //WRITE ME
 }
