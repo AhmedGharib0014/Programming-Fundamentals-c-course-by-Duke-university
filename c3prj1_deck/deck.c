c175d0772a3f89d933fa52c5e01189ee1515cee5
Coursera Learner <nobody@nowhere.nul>
2018-08-27 11:58:03 +0000

    asas

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -54,3 +54,111 @@ void assert_full_deck(deck_t * d) {
     assert(count ==1);
   }
 }
+void add_card_to(deck_t * deck, card_t c){
+  
+  card_t* c1=malloc(sizeof(*c1));
+  
+  c1->value = c.value;
+  
+  c1->suit = c.suit;
+  
+  deck ->cards= realloc(deck->cards,(deck ->n_cards + 1)*sizeof(*deck ->cards));
+  
+  deck ->cards[deck ->n_cards]=c1;
+  
+  deck ->n_cards ++;
+  
+}
+
+
+
+card_t * add_empty_card(deck_t * deck){
+  
+  card_t* c1=malloc(sizeof(*c1));
+  
+  c1->value = 0;
+  
+  c1->suit = 0;
+  
+  deck ->cards= realloc(deck->cards,(deck ->n_cards + 1)*sizeof(*deck ->cards));
+  
+  deck ->cards[deck ->n_cards]=c1;
+  
+  deck ->n_cards ++;
+  
+  return c1;
+  
+}
+
+
+
+
+
+deck_t * make_deck_exclude(deck_t * excluded_cards){
+  
+  deck_t* ans=malloc(sizeof(*ans));
+  
+  ans->cards=NULL;
+  
+  ans->n_cards=0;
+  
+  for(unsigned i=0;i<52;i++){
+    
+    card_t c= card_from_num(i);
+    
+    if(!deck_contains(excluded_cards,c)) add_card_to(ans,c);
+    
+  }
+  
+  return ans;
+  
+}
+
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) {
+  
+  deck_t* deck=malloc(sizeof(*deck));
+  
+  deck->n_cards=0;
+  
+  deck->cards=NULL;
+  
+  for (size_t i=0 ; i<n_hands ;i++){
+    
+    for(int x=0 ; x<hands[i]->n_cards ;x++){
+      
+      deck->cards=realloc(deck->cards,(deck->n_cards+1)*sizeof(*deck->cards));
+      
+      deck->cards[deck->n_cards]=hands[i]->cards[x];
+      
+      deck->n_cards ++;
+      
+    }
+    
+  }
+  
+  deck_t* deck2=make_deck_exclude(deck);
+  
+  free(deck->cards);
+  
+  free(deck);
+  
+  return deck2 ;
+  
+}
+
+
+
+void free_deck(deck_t * deck) {
+  
+  for(int i=0 ;i<deck->n_cards;i++){
+    
+    free(deck->cards[i]);
+    
+  }
+  
+  free(deck->cards);
+  
+  free(deck);
+  
+}
+
--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -200,7 +200,31 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
 //implementation in eval-c4.o) so that the
 //other functions we have provided can make
 //use of get_match_counts.
-unsigned * get_match_counts(deck_t * hand) ;
+unsigned * get_match_counts(deck_t * hand) {
+  
+  unsigned* arr=malloc(hand->n_cards*sizeof(*arr));
+  
+
+  
+  for(int i=0 ; i< hand->n_cards ; i++){
+    
+    card_t x = *(hand->cards[i]);
+    
+    unsigned  count=0;
+    
+    for(int j=0 ; j< hand->n_cards;j++){
+      
+      if(com (*hand->cards[j],x)) count ++;
+      
+    }
+    
+    arr[i] = count;}
+  
+
+  
+  return arr;
+  
+}
 
 // We provide the below functions.  You do NOT need to modify them
 // In fact, you should not modify them!
