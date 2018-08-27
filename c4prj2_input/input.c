b7f7e5ec59f53e59a00e55bd241eb18fd8bf40f5
Coursera Learner <nobody@nowhere.nul>
2018-08-27 22:29:48 +0000

    asas

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -5,38 +5,31 @@
 #include "deck.h"
 #include "cards.h"
 #include "future.h"
-
 deck_t * hand_from_string(const char * str, future_cards_t * fc){
   deck_t* deck=malloc(sizeof(*deck));
   deck->cards = NULL;
   deck->n_cards=0;
-  char arr[strlen(str)];
-  int arr_len=0;
   for(int i=0;i<strlen(str);i++){
-    if((str[i] == '\n')||(str[i] == ' ')||(str[i] == '\0'))continue;
-    arr[arr_len]=str[i];
-    arr_len ++;
-  }
-  
-  if(arr_len%2 != 0) {
-    fprintf(stderr, "wrong number of chars in hand\n" );
-    return NULL;}
-  arr[arr_len]='\0';
-    
-  int j=0;
-  while (arr[j] != '\0') {
-      if(arr[j] == '?'){
-        add_future_card(fc,arr[j+1]-'0',add_empty_card(deck)) ;
-      }else{
-        card_t x = card_from_letters(arr[j],arr[j+1]);
-         add_card_to(deck,x);
+    if((str[i] == '\n')||(str[i] == ' '))continue;
+    else{
+      if(str[i] == '?'){
+        i++;
+        char num[strlen(str)];
+        int n=0;
+        if(!((str[i] == '\n')||(str[i] == ' '))) {
+          num[n]=str[i];
+          i++;n++;}
+        num[n]='\0';
+        add_future_card(fc,atoi(num),add_empty_card(deck)) ;
       }
-      j = j+2;
+      else{
+	card_t x = card_from_letters(str[i],str[i+1]);
+	add_card_to(deck,x);
+	i++;
+      }
+    }
   }
-  if (deck->n_cards < 5 ){
-    fprintf(stderr,"asas");
-    return NULL;
-  }
+  if (deck->n_cards < 5 )return NULL;
   return deck;
 }
 
