a29a1b88609ee4fedd7807b9a6c7da7ce95bea0c
Coursera Learner <nobody@nowhere.nul>
2018-08-30 13:16:55 +0000

    dsd

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -180,6 +180,7 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
 //implementation in eval-c4.o) so that the
 //other functions we have provided can make
 //use of get_match_counts.
+
 unsigned * get_match_counts(deck_t * hand) {
   unsigned* arr=malloc(hand->n_cards*sizeof(*arr));
   for(int i=0 ; i< hand->n_cards ; i++){
--- c4prj3_finish/main.c
+++ c4prj3_finish/main.c
@@ -27,13 +27,13 @@ int win_hand(deck_t ** deck_array,int n_hands){
   for(int x=0;x<n_hands+1;x++){
     if(w[x] > w[largest])largest=x;
   }
-  int count=0;  
-  if(w[n_hands]>0){ 
-      for(int x=0;x<n_hands+1;x++){
-	if(w[x] == w[largest]) count++;
-      }
-  }
-  if(count>1) return n_hands+1;
+  //  int count=0;  
+  // if(w[n_hands]>0){ 
+  //  for(int x=0;x<n_hands+1;x++){
+  //	if(w[x] == w[largest]) count++;
+  //  }
+  // }
+  //  if(count>1) return n_hands+1;
   return  largest;  
 }
 
